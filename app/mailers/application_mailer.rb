b4b8c7c236802a52c8fd71464d9a97e8f18f0ba4
Alexey Melekhin <amelekhin@gmail.com>
2023-12-27 21:42:43 +0300

    signup form localization

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'amelekhin@gmail.com'
+  layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       get 'select_lens'
     end
   end
+  get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
